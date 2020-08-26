3e16e7c76fd2e79b8a01ebd4314a7504fe6b4382
Truetotosse <truetotosse@gmail.com>
2020-08-26 10:42:25 +0300

    Add files via upload

---
+++ XpoWebApiClient/XpoWebApiClient/Customer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DevExpress.Xpo;
+
+namespace XpoWebApiClient {
+    public class Customer : XPObject {
+        public Customer(Session session) : base(session) { }
+        private string fFirstName;
+        public string FirstName {
+            get { return fFirstName; }
+            set { SetPropertyValue(nameof(FirstName), ref fFirstName, value); }
+        }
+
+        private string fLastName;
+        public string LastName {
+            get { return fLastName; }
+            set { SetPropertyValue(nameof(LastName), ref fLastName, value); }
+        }
+        [PersistentAlias("Concat([FirstName], ' ', [LastName])")]
+        public string ContactName {
+            get { return (string)EvaluateAlias(nameof(ContactName)); }
+        }
+
+
+    }
+}
---
+++ XpoWebApiService/Controllers/XPOController.cs
@@ -0,0 +1,22 @@
+﻿using DevExpress.Xpo.DB;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace XpoWebApi {
+
+    [Route("api/[controller]")]
+    [ApiController]
+    [EnableCors("AllowAll")]
+    public class XPOController : ControllerBase {
+        private static  WebApiHelper webHelper;
+        public XPOController(IDataStore dataStore) {
+            webHelper = new WebApiHelper(dataStore);
+        }
+
+        [HttpPost("{method}")]
+        public ActionResult<string> Post(string method, [FromBody]string args) {
+            return webHelper.Handle(method, args);      
+        }
+    }    
+}
