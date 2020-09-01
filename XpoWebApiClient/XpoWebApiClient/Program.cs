1a58326272e9e0128cdf6667c8e8ffe4a777d5f9
trenin.georgii <georgii.trenin@devexpress.com>
2020-09-01 17:18:03 +0300

    2.0

--- XpoWebApiClient/XpoWebApiClient/Program.cs
+++ XpoWebApiClient/XpoWebApiClient/Program.cs
@@ -4,38 +4,31 @@ using DevExpress.Xpo.DB;
 using DevExpress.Xpo.Metadata;
 using DevExpress.Data.Filtering;
 
+
 namespace XpoWebApiClient {
     class Program {
         static void Main(string[] args) {
-             IDataStore dataStore = new WebAPIDataStore("http://localhost:65270/");
-             IDataLayer dataLayer = new SimpleDataLayer(new ReflectionDictionary(), dataStore);
-             UnitOfWork uow = new UnitOfWork(dataLayer);
-            Customer product = new Customer(uow) {
+            IDataStore dataStore = new WebAPIDataStore("http://localhost:51341/");
+            IDataLayer dataLayer = new SimpleDataLayer(new ReflectionDictionary(), dataStore);
+            UnitOfWork uow = new UnitOfWork(dataLayer);
+            Customer newCustomer = new Customer(uow) {
                 FirstName = "Customer",
                 LastName = "FoundFirst"
             };
-            
-            uow.Save(product);
-
-            product = new Customer(uow) {
+            uow.Save(newCustomer);
+            newCustomer = new Customer(uow) {
                 FirstName = "Customer",
                 LastName = "FoundSecond"
             };
-
+            uow.Save(newCustomer);
             uow.CommitChanges();
-
-            Customer search = uow.FindObject<Customer>( CriteriaOperator.Parse("FirstName=?", "Customer"));
+            Customer search = uow.FindObject<Customer>(CriteriaOperator.Parse("FirstName=?", "Customer"));
             Console.WriteLine(search.ContactName);
-            
             uow.Delete(search);
             uow.CommitChanges();
-
             search = uow.FindObject<Customer>(CriteriaOperator.Parse("FirstName=?", "Customer"));
             Console.WriteLine(search.ContactName);
-
             Console.ReadKey();
-
-
         }
     }
 }
---
+++ XpoWebApiService/WebApiService/Controller/XPOController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using DevExpress.Xpo.DB;
+using DevExpress.Xpo;
+using System.Text.Json;
+using System.Diagnostics;
+
+namespace WebApiService.Controller {
+
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class XPOController : ControllerBase {
+       
+        static IDataStore dataStore = new InMemoryDataStore();
+        static IDataLayer dataLayer = new SimpleDataLayer(dataStore);
+        static UnitOfWork uow = new UnitOfWork(dataLayer);
+        [HttpPost("modifydataasync")]
+        public ActionResult<ModificationResult> Modify( [FromBody] ModificationStatement[] args) {
+            
+            return dataStore.ModifyData(args);
+
+        }
+        [HttpPost("selectdataasync")]
+        public ActionResult<SelectedData> Select([FromBody] SelectStatement[] args) {
+
+            return dataStore.SelectData(args);
+
+        }
+        [HttpPost("updateschema/{boolArg:bool}")]
+        public ActionResult<UpdateSchemaResult> UpdateT(bool boolArg, [FromBody] DBTable[] args) {
+            
+            return dataStore.UpdateSchema(boolArg,args);
+
+        }
+       
+    }
+}
